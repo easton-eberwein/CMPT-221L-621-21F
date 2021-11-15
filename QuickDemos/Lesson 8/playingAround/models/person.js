3ca92999af1cb16c089adbe5128120e492d5ec12
Easton Eberwein <easton.eberwein1@marist.edu>
2021-11-15 15:30:32 -0500

    Delete person.js

--- QuickDemos/Lesson 8/playingAround/models/person.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const personSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String
-})
-
-module.exports = mongoose.model("Person", personSchema);
